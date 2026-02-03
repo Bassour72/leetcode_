4a119e1634d0ff0d975b7c55d1decbe8b7ee7ce4
Younes Bassour <ybassour@e2r5p2.1337.ma>
2026-02-03 09:07:48 +0100

    solving median of two arrays:

--- median_of_two_04.cpp
+++ median_of_two_04.cpp
@@ -2,53 +2,58 @@
 #include <stdlib.h>
 #include <vector>
 using namespace std;
+class Solution 
+{
+    std::vector<int> _JoinSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    {
+        std::vector<int> nums3;
+        int i;
+        int  j;
+        
+        
+        nums3.reserve(nums1.size() + nums2.size());
+        i = 0;
+        j = 0;
+        while (i < (int)nums1.size() && j < (int)nums2.size()) 
+        {
+            if (nums1[i] < nums2[j]) 
+                nums3.push_back(nums1[i++]);
+            else 
+                nums3.push_back(nums2[j++]);
+        }
+        while (i < (int)nums1.size()) 
+            nums3.push_back(nums1[i++]);
+        while (j < (int)nums2.size()) 
+            nums3.push_back(nums2[j++]);
+        return (nums3);
+    }
 
-class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
     {
-        if (nums1.size() > nums2.size()) 
-            return findMedianSortedArrays(nums2, nums1);
+        std::vector<int> nums3;
+        int isZero;
 
-        int m = nums1.size();
-        int n = nums2.size();
-        int left = 0;
-        int right = m;
-        int half = (m + n + 1) / 2;
-
-        while (left <= right)
+        nums3 = _JoinSortedArrays( nums1, nums2);
+        if ((nums3.size() % 2 == 1))
+            return (nums3[nums3.size() / 2] );
+        else
         {
-            int i = (left + right) / 2;
-            int j = half - i;
-
-            int left1 = (i > 0) ? nums1[i - 1] : INT_MIN;
-            int right1 = (i < m) ? nums1[i] : INT_MAX;
-
-            int left2 = (j > 0) ? nums2[j - 1] : INT_MIN;
-            int right2 = (j < n) ? nums2[j] : INT_MAX;
-            cout << "end\n";
-            if (left1 <= right2 && left2 <= right1)
-            {
-                if ((m + n) % 2 == 0)
-                    return (max(left1, left2) + min(right1, right2)) / 2.0;
-                else
-                    return max(left1, left2);
-            } 
-            else if (left1 > right2)
-                right = i - 1;
-            else 
-                left = i + 1;
+            isZero = nums3[(nums3.size() - 1) / 2 ];
+            if (isZero == 0)
+                return (0);
+            isZero = nums3[(nums3.size() - 1) / 2 ] + nums3[(nums3.size()) / 2 ];
+            return (((double)isZero  / 2));
 
         }
-        return 0.0;
     }
 };
 
 int main(void)
     
 {
-    vector <int> numr1 = {1,2,3,4,5,6,7,8};//{1,2,3,4,5};
-     vector <int> numr2 = {1,2,3,4,5}; //{6,7,8,9,10,11,12,13,14,15,16,17};
+    vector <int> numr1 = {2,2,4,4}; //{1,2,3,4,5,6,7,8};//{1,2,3,4,5};[1,3], nums2 = [2]
+     vector <int> numr2 = {2,2,4,4}; //{1,2,3,4,5}; //{6,7,8,9,10,11,12,13,14,15,16,17};
      Solution So;
     cout << "\n" << So.findMedianSortedArrays(numr1, numr2) << endl;
     return 0;
